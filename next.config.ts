c9f0539e4d03607f9f0f3391024a4770347ae30b
symbiosisinfra <167998162+symbiosisinfra@users.noreply.github.com>
2025-03-31 18:22:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
     workerThreads: false,
     cpus: 1,
   },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 export default nextConfig;
